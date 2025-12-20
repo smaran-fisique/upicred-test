7914f8ae6dcfd825e665fdf5186e70d61c1f9051
Smaran Challapalli <smaran@Smarans-MacBook-Pro.local>
2025-12-20 21:12:13 +0530

    Set infographic image to use 70% of viewport height

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -32,8 +32,8 @@ const Index = () => {
     const calculateImageHeight = () => {
       const viewportHeight = window.innerHeight;
       
-      // Use 85% of viewport height for the image
-      const imageHeight = viewportHeight * 0.85;
+      // Use 70% of viewport height for the image
+      const imageHeight = viewportHeight * 0.70;
       
       // Set max height
       setImageMaxHeight(imageHeight);
